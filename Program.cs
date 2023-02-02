76252aefbc165980c1cab40d493c454cab5f3d94
SYARGEY <lss1895@mail.ru>
2023-02-02 20:08:27 +0300

    Семинар 4 ДЗ 2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage} > ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int a = InputInt("Введите любое число");
+int SumCh (int a)
+{
+    int sum =0;
+    int advance =0;
+    while (a>0)
+    {
+        advance=a%10;
+        sum = sum + advance;
+        a=a/10;
+    }
+    return sum;
+}
+System.Console.WriteLine($"сумма чисел введенного числа равна {SumCh(a)}");
